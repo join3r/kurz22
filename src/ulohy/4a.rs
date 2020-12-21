a9ae9c1257e96ec0081eb34195e531afce4ad360
join3r <join3r@gmail.com>
2020-12-21 13:28:49 +0100

    fix

--- src/ulohy/4a.rs
+++ src/ulohy/4a.rs
@@ -1,4 +1,4 @@
 // Načítajte súbor 4.txt a uložte ho do vektoru tak aby každý riadok bol samostatný člen
 // použite metódy split_whitespace(), lines() a collect()
 
-// vypíšte prvé slovo z druhej vety a a druhé slovo z každej vety
+// vypíšte prvé slovo z každej vety
